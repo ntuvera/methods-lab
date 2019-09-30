5d0c04655eeff2a7ea3a60016d2c10a842c5c524
ntuvera <ntuvera@gmail.com>
2019-09-30 17:25:49 -0400

    DONE requirements, with SOuts to veryfiy behavior.  TODO: bonus filterLongWords

--- src/co/ga/nyc/Main.java
+++ src/co/ga/nyc/Main.java
@@ -10,23 +10,35 @@ public class Main {
         // write your code here
         System.out.println(isCharacterAVowel("a")); // true
         System.out.println(isCharacterAVowel("z")); // false
+
         System.out.println(maxOfTwoNumbers(0, 1));        // 1
         System.out.println(maxOfTwoNumbers(-2, -3));      // -2
         System.out.println(maxOfTwoNumbers(-2, 3));       // 3
+
         System.out.println(maxOfThreeNumbers(0, -2, 3));         // 3
         System.out.println(maxOfThreeNumbers(-3, -2, -1));       // -1
+
         System.out.println(numberOfArgs(-2, -3));               // 2
         System.out.println(numberOfArgs(-2, -3, "cat", 1.56));  // 4
+
         System.out.println(reverseString("Tested"));            // "detseT"
         System.out.println(reverseString("jag testar"));        // "ratset gaj"
+        System.out.println(reverseString("jag testar"));        // "ratset gaj"
+        System.out.println(reverseString("jag testar"));        // "ratset gaj"
 
+//        System.out.println(findLongestWord(["cat", "bird", "lizard"]));             // 6
+//        System.out.println(findLongestWord(["kangaroo", "bird", "lizard"]));        // 8
+
+//        System.out.println(filterLongWords(["cat", "bird", "lizard"], 3));          // ["cat"]
+//        System.out.println(filterLongWords(["cat", "bird", "lizard"], 5));          // ["cat", "bird"]
+//        System.out.println(filterLongWords(["cat", "bird", "lizard"], 6));          // []
     }
 
-    public static double maxOfTwoNumbers (double num1, double num2) {
+    public static double maxOfTwoNumbers(double num1, double num2) {
         return (num1 > num2) ? num1 : num2;
     }
 
-    public static double maxOfThreeNumbers (double num1, double num2, double num3) {
+    public static double maxOfThreeNumbers(double num1, double num2, double num3) {
         double largestNumber = 0;
         if ((num1 > num2) && (num1 > num3)) {
             largestNumber = num1;
@@ -38,7 +50,7 @@ public class Main {
         return largestNumber;
     }
 
-    public static boolean isCharacterAVowel (String input) {
+    public static boolean isCharacterAVowel(String input) {
         if ((input.toLowerCase() == "a") ||
                 (input.toLowerCase() == "e") ||
                 (input.toLowerCase() == "i") ||
@@ -58,7 +70,7 @@ public class Main {
     // Returns the input string reversed
     public static String reverseString(String str) {
         String reversedStr = "";
-        for(int i = (str.length()-1); i>-1; i--){
+        for (int i = (str.length() - 1); i > -1; i--) {
             reversedStr += str.charAt(i);
         }
         return reversedStr;
@@ -67,7 +79,18 @@ public class Main {
     // BONUS:
 
     // findLongestWord() takes an Array and returns the length of the longest Word }
+    public static int findLongestWord(String[] arr){
+        int highestCount = 0;
+        for( int i = 0; i<arr.length;i++){
+            if(arr[i].length() > highestCount){
+               highestCount = arr[i].length();
+            }
+        }
+
+        return highestCount;
+    }
     // filterLongWords() takes an Array of Words and a number i, returns an Array with words longer than i
+
 }
 
 
