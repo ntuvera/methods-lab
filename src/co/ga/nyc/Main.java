ed3283a107a1a141e6767c4eba5166de472aade9
ntuvera <ntuvera@gmail.com>
2019-09-30 17:14:35 -0400

    initial commit with some work done already

---
+++ src/co/ga/nyc/Main.java
@@ -0,0 +1,73 @@
+package co.ga.nyc;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import com.sun.xml.internal.ws.model.wsdl.WSDLOutputImpl;
+import org.w3c.dom.ls.LSOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(isCharacterAVowel("a")); // true
+        System.out.println(isCharacterAVowel("z")); // false
+        System.out.println(maxOfTwoNumbers(0, 1));        // 1
+        System.out.println(maxOfTwoNumbers(-2, -3));      // -2
+        System.out.println(maxOfTwoNumbers(-2, 3));       // 3
+        System.out.println(maxOfThreeNumbers(0, -2, 3));         // 3
+        System.out.println(maxOfThreeNumbers(-3, -2, -1));       // -1
+        System.out.println(numberOfArgs(-2, -3));               // 2
+        System.out.println(numberOfArgs(-2, -3, "cat", 1.56));  // 4
+        System.out.println(reverseString("Tested"));            // "detseT"
+        System.out.println(reverseString("jag testar"));        // "ratset gaj"
+
+    }
+
+    public static double maxOfTwoNumbers (double num1, double num2) {
+        return (num1 > num2) ? num1 : num2;
+    }
+
+    public static double maxOfThreeNumbers (double num1, double num2, double num3) {
+        double largestNumber = 0;
+        if ((num1 > num2) && (num1 > num3)) {
+            largestNumber = num1;
+        } else if ((num2 > num1) && (num2 > num3)) {
+            largestNumber = num2;
+        } else {
+            largestNumber = num3;
+        }
+        return largestNumber;
+    }
+
+    public static boolean isCharacterAVowel (String input) {
+        if ((input.toLowerCase() == "a") ||
+                (input.toLowerCase() == "e") ||
+                (input.toLowerCase() == "i") ||
+                (input.toLowerCase() == "o") ||
+                (input.toLowerCase() == "u")
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    // return the number of arguments passed to it when called
+    public static int numberOfArgs(Object... args) {
+        return args.length;
+    }
+
+    // Returns the input string reversed
+    public static String reverseString(String str) {
+        String reversedStr = "";
+        for(int i = (str.length()-1); i>-1; i--){
+            reversedStr += str.charAt(i);
+        }
+        return reversedStr;
+    }
+
+    // BONUS:
+
+    // findLongestWord() takes an Array and returns the length of the longest Word }
+    // filterLongWords() takes an Array of Words and a number i, returns an Array with words longer than i
+}
+
+
